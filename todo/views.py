59b0eaa3fd7c18f722a93a5a718edf3819e88afd
ender <endernagshushman@gmail.com>
2024-02-04 14:35:46 +0200

    Solution

---
+++ todo/views.py
@@ -0,0 +1,59 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+from todo.models import Task, Tags
+
+
+class TodoListView(ListView):
+    model = Task
+    context_object_name = 'tasks'
+    paginate_by = 5
+    template_name = "todo/index.html"
+
+
+class TodoCreateView(CreateView):
+    model = Task
+    fields = ("content", "tags")
+    success_url = reverse_lazy("todo:index")
+
+
+class TodoUpdateView(UpdateView):
+    model = Task
+    fields = ("content", "tags")
+    success_url = reverse_lazy("todo:index")
+
+
+class TodoDeleteView(DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:index")
+
+
+class TagsListView(ListView):
+    model = Tags
+    context_object_name = 'tags'
+    paginate_by = 5
+
+
+class TagsCreateView(CreateView):
+    model = Tags
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tags")
+
+
+class TagsUpdateView(UpdateView):
+    model = Tags
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tags")
+
+
+class TagsDeleteView(DeleteView):
+    model = Tags
+
+
+def change_finished_status(request, pk):
+    task = Task.objects.get(pk=pk)
+    task.finished = not task.finished
+    task.save()
+    return redirect("todo:index")
